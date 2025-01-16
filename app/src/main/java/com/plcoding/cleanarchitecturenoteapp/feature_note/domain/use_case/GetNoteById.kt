bc7be775807ff8df893cfa7fd446caa9844417f2
dpcks0509 <dpcks0509@gmail.com>
2025-01-16 21:33:20 +0900

    feat: NoteUseCases wrapper class 구현

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/GetNoteById.kt
@@ -0,0 +1,12 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case
+
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+
+class GetNoteById(
+    private val repository: NoteRepository
+) {
+
+    suspend operator fun invoke(id: Int) {
+        repository.getNoteById(id)
+    }
+}
